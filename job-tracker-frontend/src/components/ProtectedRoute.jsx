6b8adb6efeedcd93340400696c6a3b428b106504
sri pavithra <sripavithrapayyavula2005@gmail.com>
2026-05-30 10:25:09 +0530

    Protected few pages from accessing when not logged in

---
+++ job-tracker-frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+
+const ProtectedRoute=({children}) =>{
+    const {isAuthenticated} = useAuth()
+    return isAuthenticated ? Children : <Navigate to="/login" replace />
+}
+
+export default ProtectedRoute
